07b147b3ae31a1216fa1eda9301eb177cce3fd4e
SP <74065235+SPchalil@users.noreply.github.com>
2021-01-08 02:07:29 -0330

    modified the implementation

--- src/index.js
+++ src/index.js
@@ -5,14 +5,20 @@
 //for (var i = 0; i < board.length; i++) { 
 //    board[i] = new Array(3); 
 //} 
-
+/*
 let board = [
     [" ", " ", " "],
     [" ", " ", " "],
     [" ", " ", " "]
  ];
 
- 
+
+ for (var i = 0; i < board.length; i++){
+    for (var j = 0; j <board[0].length; j++) { 
+        board [i][j] = document.querySelectorAll('.game-cell');
+    } 
+}
+*/
 
 /*
 let currentPlayer = 'X';
@@ -250,3 +256,190 @@ function tie(){
   } 
 
  */ 
+
+ /*----------------------------------------------------------------------------*/
+
+ //declaring a 2D array
+var board = [
+    [" ", " ", " "],
+    [" ", " ", " "],
+    [" ", " ", " "]
+ ];
+
+ //console.log (board);
+ 
+
+//Grab the HTML elements 
+cellDivs = document.querySelectorAll('.game-cell');
+console.log (cellDivs);
+var display = document.getElementById('status2');
+
+//Event listener -> 9 cells
+for (cellDiv of cellDivs){
+    cellDiv.addEventListener('click', handleCellClick);
+}
+
+//Event handler ->9 cells
+var currentPlayer = 'playerX';
+
+handleCellClick = (e) =>{
+    //const classList = e.target.classList;
+    const location = e.target.classList[1];
+    //console.log(e);
+    //console.log(location);
+    //var cell = e.target.classList[2];
+    //display.innerHTML = currentPlayer; 
+    var row = Math.floor(location/3);
+    var col = location % 3;
+
+    if (e.target.classList[2]=== 'playerX' || e.target.classList[2]=== 'playerO'){
+        return;
+    }
+    if (currentPlayer==='playerX'){
+        cellDivs[location].classList.add('playerX')
+        board[row][col] = "X";    
+        console.log(board);
+        if (winner()){
+            display.innerHTML = "Winner is " + currentPlayer +" !" ; 
+            restart();
+                 
+            
+        }
+        else{
+            currentPlayer = 'playerO';
+            display.innerHTML = "Next turn: " + currentPlayer;
+        }
+        
+    }
+    else{
+        cellDivs[location].classList.add('playerO')
+        board[row][col] = "O"; 
+        console.log(board);
+        if (winner()){
+            display.innerHTML = "Winner is " + currentPlayer +" !"; 
+            restart();   
+        }
+        else{
+            currentPlayer = 'playerX';
+            display.innerHTML = "Next turn: " + currentPlayer;
+        }
+        
+    }
+    
+    //console.log(cellDivs[location]);
+    //cellDivs[location].add(currentPlayer);
+    
+
+} 
+
+/*------------------------Winning logic-----------------------------------------------------*/
+
+// A function that returns true if any of the row is crossed with the same player's move 
+function rowCrossed() 
+{ 
+	for (var i=0; i<3; i++) 
+	{ 
+		if (board[i][0] == board[i][1] && 
+			board[i][1] == board[i][2] && 
+			board[i][0] != " "){
+            return true;
+            } 
+		else {
+            return false;
+        }	 
+	} 
+	
+} 
+
+// A function that returns true if any of the column 
+// is crossed with the same player's move 
+function columnCrossed() 
+{ 
+	for (var i=0; i<3; i++) 
+	{ 
+		if (board[0][i] == board[1][i] && 
+			board[1][i] == board[2][i] && 
+			board[0][i] != " ") {
+                return true;
+            } 
+        else {
+            return false; 
+        }
+	} 
+	
+} 
+
+// A function that returns true if any of the diagonal 
+// is crossed with the same player's move 
+function diagonalCrossed() 
+{ 
+	if (board[0][0] == board[1][1] && 
+		board[1][1] == board[2][2] && 
+		board[0][0] != " "){
+        return true; 
+        } 
+		
+		
+	else if (board[0][2] == board[1][1] && 
+		board[1][1] == board[2][0] && 
+		board[0][2] != " "){
+        return true;
+        } 
+	else{
+        return false;
+    }	 
+ 
+} 
+
+// A function that returns true if there is a winner 
+function winner() { 
+    if (rowCrossed() || columnCrossed() || diagonalCrossed() ) {
+        return true;
+    }
+    else{
+        return false;
+    } 
+} 
+
+/*
+/*-------------------------------------Reset------------------------------------*/
+
+/*function reset(){
+    //currentPlayer = 'playerX';
+    //display.innerHTML = "Let's Play";
+    for (cellDiv of cellDivs){
+        cellDiv.classList.remove('playerX')
+        cellDiv.classList.remove('playerO')
+    }
+    
+}*/
+/*
+/*-------------------------------------Restart------------------------------------*/
+/*function restart(){
+    
+    display.innerHTML = "Let's Play";
+    for (var i = 0; i < board.length; i++){
+        for (var j = 0; j <board[0].length; j++) { 
+            board [i][j] = "";
+        } 
+    }
+    for (cellDiv of cellDivs){
+        cellDiv.classList.remove('playerX')
+        cellDiv.classList.remove('playerO')
+        
+    }
+      
+} */
+
+function restart(){
+    
+    setTimeout(function(){location.reload();},2000);  
+      
+}   
+
+//function myFunction() {
+//    setTimeout(function(){ alert("Hello"); }, 2000);
+//  }
+
+/*---------------------------------------Tie--------------------------*/
+
