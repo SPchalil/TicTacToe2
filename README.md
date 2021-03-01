dc7cc566fd7ffde03d310231373ef196a915c7a9
SP <74065235+SPchalil@users.noreply.github.com>
2021-03-01 13:01:44 -0330

    update readme

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# TicTacToe2
+# Game - TicTacToe
+
+A Tic-Tac-Toe game developed using pure HTML, CSS and Javascript.
  
+This project is a second version of the Tic-Tac-Toe game developed as part of learning and practicing Javascript as well as implementing modern Javascript notations (ESX). 
+The project makes use of the two dimensional array, that simplified the implemention of game logic. 
+Also, to make the game environment more attracting for kids to play, the game board is made more colourful using CSS styling.
+
+### How to run
+
+The website is hosted on Github Pages, https://spchalil.github.io/TicTacToe2/. You can also clone the repository and use LiveServer in VSCode to run index.html and check out an offline version on your local machine.
--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
-/*----------------------------------------------------------------------------*/
+/*-------------------------------Game logic------------------------------------*/
 //Initializing an empty 2D board array to store 'X' and 'O' 
 var board = [
     [" ", " ", " "],
     [" ", " ", " "],
     [" ", " ", " "]
- ];
+];
 
 /*----------------------------------------------------------------------------*/
 //Grab the HTML elements 
 cellDivs = document.querySelectorAll('.game-cell');
-console.log (cellDivs);
+console.log(cellDivs);
 var display = document.getElementById('status2');
 
 //Event listener -> 9 cells
-for (cellDiv of cellDivs){
+for (cellDiv of cellDivs) {
     cellDiv.addEventListener('click', handleCellClick);
 }
 
@@ -23,158 +23,153 @@ for (cellDiv of cellDivs){
 var currentPlayer = 'playerX';
 var count = 1;
 
-handleCellClick = (e) =>{
-const location = e.target.classList[1];
+handleCellClick = (e) => {
+    const location = e.target.classList[1];
     //console.log(e);
     //console.log(location);
-const cell = e.target.classList[2];
-let row = Math.floor(location/3);
-let col = location % 3;
+    const cell = e.target.classList[2];
+    let row = Math.floor(location / 3);
+    let col = location % 3;
 
-if (cell=== 'playerX' || cell=== 'playerO'){
-    return;
-}
+    if (cell === 'playerX' || cell === 'playerO') {
+        return;
+    }
 
-/*-----------------------------Play Game----------------------------------------*/
+    /*-----------------------------Play Game----------------------------------------*/
 
     console.log(count);
-    if (count<=9){
+    if (count <= 9) {
         //Check Player
-        if (currentPlayer==='playerX'){
+        if (currentPlayer === 'playerX') {
             cellDivs[location].classList.add('playerX')
-            board[row][col] = "X";    
+            board[row][col] = "X";
             console.log(board);
-            if(count<9){
-            if (winner()){     //Winner
-                display.innerHTML = `Winner is ${currentPlayer} ✌!`; 
-                reset();   
+            if (count < 9) {
+                if (winner()) {     //Winner
+                    display.innerHTML = `Winner is ${currentPlayer} ✌!`;
+                    reset();
                 }
-        
-            else{
-                currentPlayer = 'playerO';  //Next Turn
-                display.innerHTML = `Next turn: ${currentPlayer}`;
+
+                else {
+                    currentPlayer = 'playerO';  //Next Turn
+                    display.innerHTML = `Next turn: ${currentPlayer}`;
                 }
             }
-            else if (count ===9){
-                if (winner()){   //Winner
-                    display.innerHTML = `Winner is ${currentPlayer} ✌!`; 
-                    reset();   
-                    }
-                else{
+            else if (count === 9) {
+                if (winner()) {   //Winner
+                    display.innerHTML = `Winner is ${currentPlayer} ✌!`;
+                    reset();
+                }
+                else {
                     display.innerText = "Match Draw !"; //Tie
                 }
             }
         }
         else {
             cellDivs[location].classList.add('playerO')
-            board[row][col] = "O"; 
+            board[row][col] = "O";
             console.log(board);
-            if(count<9){
-            if (winner()){    //Winner
-                display.innerHTML = `Winner is ${currentPlayer} ✌!`; 
-                reset();   
+            if (count < 9) {
+                if (winner()) {    //Winner
+                    display.innerHTML = `Winner is ${currentPlayer} ✌!`;
+                    reset();
                 }
 
-            else{
-                currentPlayer = 'playerX';  //Next Turn
-                display.innerHTML = `Next turn: ${currentPlayer}`;
+                else {
+                    currentPlayer = 'playerX';  //Next Turn
+                    display.innerHTML = `Next turn: ${currentPlayer}`;
                 }
             }
-            else if (count ===9){
-                if (winner()){   //Winner
-                    display.innerHTML = `Winner is ${currentPlayer} ✌!`; 
-                    reset();   
-                    }
-                else{
+            else if (count === 9) {
+                if (winner()) {   //Winner
+                    display.innerHTML = `Winner is ${currentPlayer} ✌!`;
+                    reset();
+                }
+                else {
                     display.innerText = "Match Draw !"; //Tie
                 }
             }
-        
-        
+
+
         }
-    
-    count++;
+
+        count++;
     }
-    
+
 }
-   
+
 /*------------------------Winning logic---------------------------------------------*/
 
 // A function that returns true if any of the row 
 //is crossed with the same player's move 
-function rowCrossed() 
-{ 
-	for (let i=0; i<3; i++) 
-	{ 
-        
-		if (board[i][0] == board[i][1] && 
-			board[i][1] == board[i][2] && 
-			board[i][0] != " "){
-            return true;
-            } 
+function rowCrossed() {
+    for (let i = 0; i < 3; i++) {
 
-	} 
-	
-} 
+        if (board[i][0] == board[i][1] &&
+            board[i][1] == board[i][2] &&
+            board[i][0] != " ") {
+            return true;
+        }
+
+    }
+
+}
 
 // A function that returns true if any of the column 
 // is crossed with the same player's move 
-function columnCrossed() 
-{ 
-	for (let i=0; i<3; i++) 
-	{ 
-		if (board[0][i] == board[1][i] && 
-			board[1][i] == board[2][i] && 
-			board[0][i] != " ") {
-                return true;
-            } 
-        
-	} 
-	
-} 
+function columnCrossed() {
+    for (let i = 0; i < 3; i++) {
+        if (board[0][i] == board[1][i] &&
+            board[1][i] == board[2][i] &&
+            board[0][i] != " ") {
+            return true;
+        }
+
+    }
+
+}
 
 // A function that returns true if any of the diagonal 
 // is crossed with the same player's move 
-function diagonalCrossed() 
-{ 
-	if (board[0][0] == board[1][1] && 
-		board[1][1] == board[2][2] && 
-		board[0][0] != " "){
-        return true; 
-        } 
-		
-		
-	else if (board[0][2] == board[1][1] && 
-		board[1][1] == board[2][0] && 
-		board[0][2] != " "){
-        return true;
-        } 
-	
- 
-} 
-
-// A function that returns true if there is a winner 
-function winner() { 
-    if (rowCrossed() || columnCrossed() || diagonalCrossed() ) {
+function diagonalCrossed() {
+    if (board[0][0] == board[1][1] &&
+        board[1][1] == board[2][2] &&
+        board[0][0] != " ") {
         return true;
     }
-    
-    else{
+
+
+    else if (board[0][2] == board[1][1] &&
+        board[1][1] == board[2][0] &&
+        board[0][2] != " ") {
+        return true;
+    }
+
+
+}
+
+// A function that returns true if there is a winner 
+function winner() {
+    if (rowCrossed() || columnCrossed() || diagonalCrossed()) {
+        return true;
+    }
+
+    else {
         return false;
-    } 
-} 
+    }
+}
 
 /*------------------------------------Reset/Restart Game-------------------------*/
-function reset(){
-    setTimeout(function(){location.reload();},1000);  
+function reset() {
+    setTimeout(function () { location.reload(); }, 1000);
 }
 
 
-function restart(){
-    
-    setTimeout(function(){location.reload();},100);  
-      
-}   
+function restart() {
+
+    setTimeout(function () { location.reload(); }, 100);
+
+}
 
 /*-------------------------------------------------------------------------------*/
 
